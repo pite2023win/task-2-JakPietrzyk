ea30e48718ccf83df48250d94611f201b0dbbc9e
Jakub Pietrzyk <pietrzykj9@gmail.com>
2023-10-24 13:35:34 +0200

    initial

--- task.py
+++ task.py
@@ -24,3 +24,52 @@
 #
 #Delete these comments before commit!
 #Good luck.
+import logging
+
+class Client:
+    def __init__(self, id, name, surname):
+        self.id = id
+        self.name = name
+        self.surname = surname
+    
+    def print_info(self):
+        print(f"Client: {self.name} {self.surname}")
+
+class ClientBankEntity(Client):
+    def __init__(self, client, amount_of_money):
+            super().__init__(client.id, client.name, client.surname)
+            self.__amount_of_money = amount_of_money
+    
+    def withdraw_money(self, amount_to_withdraw):
+        self.__amount_of_money -= amount_to_withdraw
+        print(f"Recieved: {amount_to_withdraw}")
+        
+
+class Bank:
+    def __init__(self):
+        self.__bankid = 1
+        self.__clients_list = {}
+
+    def add_client(self, client, amount_of_money):
+        self.__clients_list[client.id] = ClientBankEntity(client, amount_of_money)
+
+    def print_all_clients(self):
+        for client in self.__clients_list.values():
+            client.print_info()
+
+    def withdraw_money(self, client_id, amount_to_withdraw):
+        self.__clients_list[client_id].withdraw_money(amount_to_withdraw)
+
+    def send_money_from_one_to_another(self, sender_id, reciever_id, amount_to_send):
+        pass
+        
+
+class BankFactory:
+    pass
+
+if __name__ == "__main__":
+    bank = Bank()
+    client1 = Client(1, "New", "Client")
+    bank.add_client(client1, 1000)
+    bank.print_all_clients()
+    bank.withdraw_money(1, 100)
